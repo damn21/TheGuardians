5e54c190ffa6df803efc6f4b9362ac679868cd10
damn21 <dmezae@uninorte.edu.co>
2022-10-26 03:07:30 -0500

    :hammer: Trabajando en controladores: Agregando Endpoints

---
+++ TheGuardians/Controllers/HeroesController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using TheGuardians.DBContext;
+using TheGuardians.DTOs;
+using TheGuardians.Models;
+
+namespace TheGuardians.Controllers
+{
+    [ApiController]
+    [Route("api/heroes")]
+    public class HeroesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public HeroesController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost("AgregarHeroe")]
+        public async Task<ActionResult> AgregarHeroe([FromBody] HeroeCreationDTO heroeCreationDTO)
+        {
+            var existeHeroeConElMismoApodo = await context.Personas
+                .AnyAsync(x => x.Apodo == heroeCreationDTO.IdPersonaNavigation.Apodo);
+
+            if (existeHeroeConElMismoApodo)
+            {
+                return BadRequest($"Ya existe un heroe con el mismo apodo " +
+                    $"{heroeCreationDTO.IdPersonaNavigation.Apodo}");
+            }
+
+            var heroe = mapper.Map<Heroe>(heroeCreationDTO);
+            context.Add(heroe);
+            await context.SaveChangesAsync();
+
+            var heroeDTO = mapper.Map<HeroeDTO>(heroe);
+
+            return new JsonResult(heroeDTO);
+
+        }
+
+        [HttpGet]
+        public async Task<List<HeroeDTO>> GetHeroes()
+        {
+            var heroes = await context.Heroes.Include(x => x.IdPersonaNavigation).ToListAsync();
+            return mapper.Map<List<HeroeDTO>>(heroes);
+        }
+
+        [HttpGet]
+        [Route("nombre")]
+        public async Task<ActionResult<List<HeroeDTO>>> ObtenerHeroeNombre(string nombre)
+        {
+            var heroes = await context.Heroes.Include(x => x.IdPersonaNavigation)
+                .Where(y => y.IdPersonaNavigation.Nombre.Contains(nombre)).ToListAsync();
+
+            if (heroes == null)
+            {
+                return NotFound();
+            }
+
+            return mapper.Map<List<HeroeDTO>>(heroes);
+        }
+
+        [HttpGet]
+        [Route("habilidad")]
+        public async Task<ActionResult<List<HeroeDTO>>> ObtenerHeroeHabilidad(string habilidad)
+        {
+            var heroes = await context.Heroes.Include(x => x.IdPersonaNavigation)
+                .Where(y => y.IdPersonaNavigation.HabilidadPoder.Contains(habilidad)).ToListAsync();
+
+            if (heroes == null)
+            {
+                return NotFound();
+            }
+
+            return mapper.Map<List<HeroeDTO>>(heroes);
+        }
+
+        [HttpGet]
+        [Route("ContactoPersonal")]
+        public async Task<ActionResult<ContactoPersonal>> ObtenerHeroeContacto(string contacto)
+        {
+            var contactoPersonal = await context.ContactoPersonals.Include(y => y.Heroe.IdPersonaNavigation)
+                .FirstOrDefaultAsync(x => x.Nombre.Contains(contacto));
+
+            if (contactoPersonal == null)
+            {
+                return NotFound();
+            }
+
+            return contactoPersonal;
+        }
+
+    }
+}
---
+++ TheGuardians/Controllers/PatrocinadorController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheGuardians.DBContext;
+using TheGuardians.DTOs;
+using TheGuardians.Models;
+
+namespace TheGuardians.Controllers
+{
+    [ApiController]
+    [Route("api/patrocinador")]
+    public class PatrocinadorController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public PatrocinadorController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost("Agregar patrocinador")]
+        public async Task<ActionResult> Post([FromBody] PatrocinadorCreationDTO patrocinadorCreationDTO)
+        {
+            var patrocinador = mapper.Map<Patrocinador>(patrocinadorCreationDTO);
+            context.Add(patrocinador);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<List<PatrocinadorDTO>> GetAll()
+        {
+            var patrocinadores = await context.Patrocinadors.
+                Include(x => x.Heroe.IdPersonaNavigation).ToListAsync();
+            return mapper.Map<List<PatrocinadorDTO>>(patrocinadores);
+
+        }
+
+    }
+}
